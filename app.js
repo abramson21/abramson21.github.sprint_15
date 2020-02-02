ed71eafc9ea318a6ccec4fee61ca49659184ff4b
abramson21 <56224786+abramson21@users.noreply.github.com>
2020-02-02 17:27:00 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -50,7 +50,12 @@ app.post('/signup', celebrate({
   }),
 }), createUser);
 
-app.post('/signin', loginValidator, login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }).unknown(true),
+}), login);
 
 
 app.use('/', auth, users);
